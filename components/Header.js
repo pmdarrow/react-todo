66de55252ae8ed35283ab31934fc1c48d1bc485d
Peter Darrow <pmdarrow@gmail.com>
2017-06-17 11:06:45 -0400

    Add global text and link styling

--- components/Header.js
+++ components/Header.js
@@ -6,6 +6,11 @@ export default () =>
       <style global jsx>{`
         body {
           font-family: "Helvetica Neue", Arial, sans-serif;
+          color: ##5d5d5d;
+        }
+        a {
+          text-decoration: none;
+          color: #03a9f4;
         }
       `}</style>
     </Head>
