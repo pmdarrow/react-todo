8520af084ed87b946256d30319461d14a8832467
Peter Darrow <pmdarrow@gmail.com>
2017-06-17 11:34:17 -0400

    Add link to GitHub project

--- components/Title.js
+++ components/Title.js
@@ -1,12 +1,6 @@
 export default () =>
   <div>
-    <h1>
-      react-todo{' '}
-      <small>by <a href="https://github.com/pmdarrow">pmdarrow</a></small>
-    </h1>
-    <style>{`
-      h1 {
-        margin: 2rem;
-      }
-    `}</style>
+    <h1>react-todo</h1>
+    <p>By <a href="https://github.com/pmdarrow">@pmdarrow</a></p>
+    <p><a href="https://github.com/pmdarrow/react-todo">View on GitHub</a></p>
   </div>;
