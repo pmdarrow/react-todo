15252fe9576a0045ac31a5cf46de663bf25de3ba
Peter Darrow <pmdarrow@gmail.com>
2017-06-21 02:16:51 -0300

    Use redux-actions handleActions to build todos reducer

--- reducers/todos.js
+++ reducers/todos.js
@@ -1,10 +1,11 @@
+import { handleActions } from 'redux-actions';
+import update from 'immutability-helper';
 import {
   ADD_TODO,
   EDIT_TODO,
   DELETE_TODO,
   TOGGLE_TODO,
 } from '../constants/actionTypes';
-import update from 'immutability-helper';
 
 let initialState = [{
   id: 0,
@@ -16,53 +17,53 @@ function getIndex(state, action) {
   return state.findIndex(item => item.id === action.payload.id);
 }
 
-export default function(state = initialState, action) {
-  switch (action.type) {
-    case ADD_TODO:
-      // Only add a todo if there are none or the last one isn't empty
-      if (state.length === 0 || state[state.length - 1].value !== '') {
-        return update(state, {
-          $push: [
-            {
-              id: action.payload.id,
-              value: '',
-              completed: false,
-            },
-          ],
-        });
-      }
-      return state;
-
-    case EDIT_TODO:
+export default handleActions({
+  ADD_TODO: (state, action) => {
+    // Only add a todo if there are none or the last one isn't empty
+    if (state.length === 0 || state[state.length - 1].value !== '') {
       return update(state, {
-        // TODO: May be simpler to use map() instead of immutability-helper
-        [getIndex(state, action)]: {
-          value: {
-            $set: action.payload.value,
+        $push: [
+          {
+            id: action.payload.id,
+            value: '',
+            completed: false,
           },
-        },
+        ],
       });
+    }
+    return state;
+  },
 
-    case DELETE_TODO:
-      // Ensure there's always at least one todo
-      if (state.length > 1) {
-        return update(state, {
-          $splice: [[getIndex(state, action), 1]],
-        });
-      }
-      return state;
+  EDIT_TODO: (state, action) => {
+    return update(state, {
+      // TODO: May be simpler to use map() instead of immutability-helper
+      [getIndex(state, action)]: {
+        value: {
+          $set: action.payload.value,
+        },
+      },
+    });
+  },
 
-    case TOGGLE_TODO:
-      let todoIndex = getIndex(state, action);
+  DELETE_TODO: (state, action) => {
+    // Ensure there's always at least one todo
+    if (state.length > 1) {
       return update(state, {
-        [todoIndex]: {
-          completed: {
-            $set: !state[todoIndex].completed,
-          },
-        },
+        $splice: [[getIndex(state, action), 1]],
       });
+    }
+    return state;
+  },
 
-    default:
-      return state;
+  TOGGLE_TODO: (state, action) => {
+    let todoIndex = getIndex(state, action);
+    return update(state, {
+      [todoIndex]: {
+        completed: {
+          $set: !state[todoIndex].completed,
+        },
+      },
+    });
   }
-}
+
+}, initialState);
